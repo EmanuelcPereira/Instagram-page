5e1464635d95cb9f026eeffb7e0a758776a0934f
EmanuelcPereira <emanuelcdpr@gmail.com>
2020-11-24 11:28:40 -0300

    Página inicial modificada e adicionado interação JS

---
+++ script.js
@@ -0,0 +1,6 @@
+function login (){
+    var login = document.getElementById('login')
+    var password = document.getElementById('password')
+    var res = document.getElementById('res')
+    res.innerHTML = 'Sorry, your password was incorrect. <br> Please double-check your password.'
+}
